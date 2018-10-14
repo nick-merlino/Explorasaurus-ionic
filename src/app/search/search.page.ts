c7e5271243c821237253841a6fc186494061c2db
Nick Merlino <nick@merlino.net>
2018-10-14 04:57:18 -0400

    Filter list is close

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-search',
   templateUrl: 'search.page.html',
   styleUrls: ['search.page.scss']
 })
-export class SearchPage {}
+export class SearchPage {
+  date = new Date().toISOString();
+  sourceSearchTerm: String = '';
+  sourceOptions;
+  locs = ["hi", "there"]
+
+  ionViewDidLoad() {
+       this.setFilteredItems();
+   }
+
+   setFilteredItems() {
+       this.sourceOptions = this.locs.filter((city) => city.startsWith(this.sourceSearchTerm));
+   }
+}
